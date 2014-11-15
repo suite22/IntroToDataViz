77c8968e1b55fbc2ff3eebbca4aefb1ff0e5e068
BoRyan Eberhard <github@riverseeker.com>
2014-11-15 14:39:46 -0800

    map now updates when city is changed

--- course/final/js/main.js
+++ course/final/js/main.js
@@ -1,9 +1,53 @@
 var map = new Datamap({
-	 element: document.getElementById("usmap"),
-	 scope: 'usa'
+    element: document.getElementById('usmap'),
+    scope: 'usa',
+    geographyConfig: {
+        popupOnHover: false,
+        highlightOnHover: false
+    },
+    bubbleConfig: {
+        borderColor: '#35A4FF',
+        highlightFillOpacity: 0.25
+    }
 });
 
-d3.select("#city").on("input", function() {
-	console.log(this.value);
+var jsonCities = d3.json("data/cities.json", function(cityData) {
 
-});
+    var cities = cityData.map(function(row) {
+        //debugger;
+        return {
+            latitude: row.la,
+            longitude: row.lo,
+            city: row.ci,
+            radius: 2
+        }
+    });
+    
+    var citiesSample = _.sample(cities, 1000);
+
+    map.bubbles(citiesSample, {
+        popupTemplate: function (geo, d) {
+            return ['<div class="hoverinfo">' + d.city,
+            '</div>'].join('');
+        }
+    })
+
+	d3.select("#city").on("input", function() {
+		var searchCity = this.value.toLowerCase();
+
+		console.log(this.value);
+
+		citiesSample = _.sample(
+			_.filter(cities, function(row) {
+				var city = row.city.toLowerCase();
+				return (city.indexOf(searchCity) == 0);
+			})
+		, 1000);
+		
+		debugger;
+		map.bubbles(citiesSample);
+
+	});
+})            
+
+

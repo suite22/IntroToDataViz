17e08dde638192fbd35e24d1a662af9e936eacee
Ben Goertz <bengoertz@gmail.com>
2014-11-15 15:39:11 -0800

    Merging BoRyan's changes into mine.

--- course/final/js/mainBen.js
+++ course/final/js/mainBen.js
@@ -1,47 +1,87 @@
-var cities = [];
+(function() {
+    var map = new Datamap({
+         element: document.getElementById("usmap"),
+         scope:"usa",
+         geographyConfig: {
+              popupOnHover:false,
+              highlightOnHover:false
+         },
+         bubbleConfig: {
+              borderColor:'#35A4FF',
+              highlightFillOpacity:0.25
+         }
+    });
 
-var map = new Datamap({
-    element: document.getElementById('usmap'),
-    scope: 'usa',
-    geographyConfig: {
-        popupOnHover: false,
-        highlightOnHover: false
-    },
-    bubbleConfig: {
-        borderColor: '#35A4FF',
-        highlightFillOpacity: 0.25
-    }
-});
-
-var jsonCities = d3.json("data/cities-short.json", function(cityData) {
-    //debugger;
-
-    citiesD3 = cityData.map( function(row) {
-        //debugger;
+    var jsonCities = d3.json("data/cities.json", function(data) {
+        var timer = new Timer().start(),
+            allCities = [],
+            cities = [];
     
-        return {
-            latitude: row.la,
-            longitude: row.lo,
-            city: row.ci,
-            radius: 2
-        }
-    })
+        allCities = _.map(data, function(row) {
+            return {
+                latitude: row.la,
+                longitude: row.lo,
+                city: row.ci,
+                radius: 2
+            }
+        });
+     
+        cities = _.sample(allCities, 1000);
 
-    map.bubbles(citiesD3, {
-        popupTemplate: function (geo, data) {
-            return ['<div class="hoverinfo">' + data.city,
-            '</div>'].join('');
-        }
-    })
-})            
+        updateMap(cities);
+    
+        timer.stop();
+        updateStats();
 
-d3.select("#city").on("input", function() {
-	console.log(this.value);
+        d3.select("#city").on("input", function() {
+            var searchCity = this.value.toLowerCase();
+            timer.reset().start();
 
-	// as the user updates the search field, create a new filtered selection
-	// then update the map. 
-	var userSearch = jsonCities.filter( function(d, i) { 
-                return str2Date(d.Year) > str2Date("1900");
+            cities = _.sample(
+                _.filter(allCities, function(row) {
+                    var city = row.city.toLowerCase();
+                    return (city.indexOf(searchCity) == 0);
                 })
-	console.log(userSearch);
-});
+            , 1000);
+        
+            updateMap(cities);
+            timer.stop();
+            updateStats();
+        });
+
+        function updateStats() {
+            d3.selectAll(".execution_time").text(timer.time);
+            d3.selectAll(".record_count").text(cities.length);
+        }
+    });
+
+    /* Functions */
+    function updateMap(data) {
+        map.bubbles(data, {
+            popupTemplate: function (geo, d) {
+                return ['<div class="hoverinfo">' + d.city,
+                '</div>'].join('');
+            }
+        });
+    } 
+
+    /* Timer Class */
+    function Timer() {
+        var start, end;
+        this.reset = function() {
+            start = end = new Date().getTime();
+            return this;
+        }
+        this.start = function() {
+            start = new Date().getTime();
+            return this;
+        }
+        this.stop = function() {
+            end = new Date().getTime();
+            this.time = end - start;
+            return this;
+        }
+        this.time = 0;
+        this.reset();       
+    }
+})();

03370c642ab353fe4659478f1fc895175383f0d1
BoRyan Eberhard <github@riverseeker.com>
2014-11-15 16:35:19 -0800

    more refinements

--- course/final/js/main.js
+++ course/final/js/main.js
@@ -8,16 +8,62 @@
 		 },
 		 bubbleConfig: {
 			  borderColor:'#35A4FF',
-			  highlightFillOpacity:0.25
+			  highlightFillOpacity:0.25,
+			  highlightOnHover:true,
+			  popupOnHover:true
 		 }
 	});
 
 	var jsonCities = d3.json("data/cities.json", function(data) {
-		var timer = new Timer().start(),
+		var timer = new Timer(),
 			allCities = [],
 			cities = [];
 	
-		allCities = _.map(data, function(row) {
+		// format the data for the topojson format
+		allCities = formatToDataMaps(data);
+	 
+	 	// update bubbles on map
+		filterCities(allCities);
+
+		// event handler for when city name is changed
+		d3.select("#city").on("input", function() {
+			filterCities(allCities, this.value);
+		});
+	});
+
+	/*** Functions ***/
+	
+	/* updates the data to only show cities beginning with "beginsWith" */
+	function filterCities(data, beginsWith) {
+		var timer = new Timer(),
+			cities = [];
+		
+		if(beginsWith && beginsWith.length > 0) {
+			// we have a city search string
+			cities = sampleData(
+				_.filter(data, function(row) {
+					var city = row.city.toLowerCase();
+					return (city.indexOf(beginsWith.toLowerCase()) == 0);
+				})
+			);
+		} else {
+			// no city search string
+			cities = sampleData(data);
+		}
+
+		// check to see if sample size is < 10
+		//cities = _.map
+
+		// refresh bubbles on map	
+		updateDataMap(cities);
+		
+		// update text stats at bottom of map		
+		updateStats(cities, timer);	
+	}
+	
+	/* formats json data to DataMaps format */
+	function formatToDataMaps(data) {
+		return _.map(data, function(row) {
 			return {
 				latitude: row.la,
 				longitude: row.lo,
@@ -25,38 +71,16 @@
 				radius: 2
 			}
 		});
-	 
-		cities = _.sample(allCities, 1000);
-
-		updateMap(cities);
+	}
 	
-		timer.stop();
-		updateStats();
-
-		d3.select("#city").on("input", function() {
-			var searchCity = this.value.toLowerCase();
-			timer.reset().start();
-
-			cities = _.sample(
-				_.filter(allCities, function(row) {
-					var city = row.city.toLowerCase();
-					return (city.indexOf(searchCity) == 0);
-				})
-			, 1000);
-		
-			updateMap(cities);
-			timer.stop();
-			updateStats();
-		});
-
-		function updateStats() {
-			d3.selectAll(".execution_time").text(timer.time);
-			d3.selectAll(".record_count").text(cities.length);
-		}
-	});
-
-	/* Functions */
-	function updateMap(data) {
+	/* reduces dataset size, so as not to overwhelm the map with too many data points */
+	function sampleData(data) {
+		// todo: try to add population
+		return _.sample(data, 1000);
+	}
+	
+	/* calls the DataMap library to refresh the bubbles on map */
+	function updateDataMap(data) {
 		map.bubbles(data, {
 			popupTemplate: function (geo, d) {
 				return ['<div class="hoverinfo">' + d.city,
@@ -64,6 +88,13 @@
 			}
 		});
 	} 
+
+	/* updates the text statistics below the graph */
+	function updateStats(data, timer) {
+		timer.stop();
+		d3.selectAll(".execution_time").text(timer.time);
+		d3.selectAll(".record_count").text(data.length);
+	}
 
 	/* Timer Class */
 	function Timer() {
